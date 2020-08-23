3e81d162b7b18df0f106174bc932bcfabe97cbfc
David Hermoso <hermosoj@ryanair.com>
2020-08-23 19:47:22 +0200

    Add worflow

--- index.js
+++ index.js
@@ -3,18 +3,30 @@ const github = require('@actions/github');
 const https = require('https');
 const fs = require('fs');
 
+function getUpdateDateTime() {
+}
+
 try {
     https.get('https://api.elpais.com/ws/LoteriaNavidadPremiados?n=resumen', (resp) => {
         let data = '';
+        let parsedData = '';
+        let updateDateTime = '';
+        let resultsYear = '';
 
         resp.on('data', (chunk) => {
             data += chunk;
         });
 
         resp.on('end', () => {
-            data = data.replace('premios=', '')
+            data = data.replace('premios=', '');
+            parsedData = JSON.parse(data);
+            updateDateTime = parsedData.timestamp;
+            resultsYear = new Date(updateDateTime * 1000).getFullYear();
+            console.log(resultsYear)
+            console.dir(data)
 
-            fs.writeFile(`./results-${new Date().getFullYear()}.json`, data, () => {
+
+            fs.writeFile(`./results-${resultsYear}.json`, data, () => {
                 console.log('File saved!')
             })
         });
